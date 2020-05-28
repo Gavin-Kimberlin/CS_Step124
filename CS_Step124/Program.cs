b2b2d0f607d51737fd33ec0cd32003132bdf3a08
Gavin Kimberlin <guviner@gmail.com>
2020-05-28 12:37:50 -0700

    Create a program with boolean comparisons.

--- CS_Step124/Program.cs
+++ CS_Step124/Program.cs
@@ -10,6 +10,29 @@ namespace CS_Step124
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i <= number)
+            {
+                Console.WriteLine(i);
+                i += 1;
+            }
+
+            Console.WriteLine("Enter another number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int x = 0;
+            do
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+                x += 1;
+            }
+
+            while (x <= number2);
+            Console.ReadLine();
         }
     }
 }
